3e1aaaffb4c104ad257f4782464a493d1295bc07
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 19:38:52 +0100

    Update EnemyProjectile.cs

--- Scripts/Enemy/EnemyProjectile.cs
+++ Scripts/Enemy/EnemyProjectile.cs
@@ -1,7 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// this class create a projectile that the enemy shoot to target the player
+// it inherites form collidable as it will need to check for collsions agenst the player
 public class EnemyProjectile : Collidable
 {
     // Start is called before the first frame update
@@ -12,7 +13,7 @@ public class EnemyProjectile : Collidable
     public int damage;
     public float pushForce;
 
-
+    // we override the start method form collidable and run the base method we also store the player psotion in a veriablea called position ans store the  x,y corridnates in a verctor 2 variable
     protected override void Start()
     {
         base.Start();
@@ -20,11 +21,11 @@ public class EnemyProjectile : Collidable
         var position = PlayerTransform.position;
         traget = new Vector2(position.x, position.y);
     }
-
+    // in the update method that is called every frame we move the projectile toward the player uisng the method MoveToward that target
     protected override void Update()
     {
         
-        transform.position = Vector2.MoveTowards(transform.position,traget,speed*Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position,traget,speed*Time.deltaTime); // when the projectile reaches the target it is destroyed 
         if (transform.position.x == traget.x && transform.position.y == traget.y)
         {
             DestroyProjectile();
