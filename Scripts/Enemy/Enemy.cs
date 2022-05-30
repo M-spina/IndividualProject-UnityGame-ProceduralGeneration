62e4f257e463b8c1b15347ae1578d60d0cb14338
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 18:32:52 +0100

    Update Enemy.cs

--- Scripts/Enemy/Enemy.cs
+++ Scripts/Enemy/Enemy.cs
@@ -2,7 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// this is the enemy class that inherites form mover class that handles movement and damage 
 public class Enemy : Mover
 {
     // exp points
@@ -24,7 +24,8 @@ public class Enemy : Mover
     private Collider2D[] hits = new Collider2D[10];
     
     
-
+// this method is called first during run time  and assigns impotant variables, such as assing the current player psotion in the game world 
+// storring a starting position for the enemy and getting its hitbox for collision
     protected override void Start()
     {
         base.Start();
@@ -73,10 +74,10 @@ public class Enemy : Mover
             {
                 continue;
             }
-            // if any go bad this is prob problem
+            // if the colider in the array is of tag player and has the name of player we know he have hit the player 
             if (hits[i].CompareTag("Player") && hits[i].name == "Player")
             {
-                collideWithPlayer = true;
+                collideWithPlayer = true; // we set the veribale to be true and this cause the enmey to be damaged form a method in fighter that the enemy inherits through mover
             }
 
             hits[i] = null;
@@ -84,7 +85,7 @@ public class Enemy : Mover
         
     }
 
-    protected override void Death()
+    protected override void Death() // when the death method is called it destroy the object and give exp to the player through the game manager  and this diplays the exp as text in the world
     {
         base.Death();
         Destroy(gameObject);
