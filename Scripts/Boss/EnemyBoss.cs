3923515bc76a654b27eca9618095a49a7450a29c
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 17:38:27 +0100

    Update EnemyBoss.cs

--- Scripts/Boss/EnemyBoss.cs
+++ Scripts/Boss/EnemyBoss.cs
@@ -2,14 +2,16 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// the enemy boss class controlls the movement of the orb object around the enmey 
 public class EnemyBoss : Enemy
 {
+    // we take in list of floast for speed and siatnce form the enemy
+    // we also take transform components of the orbs  
     public float[] ObjectSpeed = {2.7f,-2.7f};
     public float[] distance = {0.25f,0.5f};
     public Transform[] Object;
-
-    private void Update() // The boss would have object orbiting around the it self 
+    // on every frame  
+    private void Update() // The boss would have object orbiting around the it self uisng sin and cos operators to move
     {
         for(int i = 0; i< Object.Length; i++)
         {
