e41b00cb5a7af1a0adec61f3b24b8dc8995d7a1a
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 20:02:28 +0100

    Update HealingArea.cs

--- Scripts/HealIngArea/HealingArea.cs
+++ Scripts/HealIngArea/HealingArea.cs
@@ -1,20 +1,21 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// this class inherites from Collidable to check for collision
+// this class is used to create a position that would heal the player when collided 
 public class HealingArea : Collidable
 {
-    public int healingAmount = 1;
-    private float helaingCoolDown = 1.0f;
+    public int healingAmount = 1; // we create variables for the healing amountwe want to heal the player and we also want to avoid healing the player every frame 
+    private float helaingCoolDown = 1.0f; // so we implmented a healing cool down variable 
     private float lastHeal;
 
-
-    protected override void OnCollide(Collider2D coll)
+    // the overriden method check to see if the player has collided with the object  
+    protected override void OnCollide(Collider2D coll) 
     {
        if(coll.name != "Player")
            return;
         
-        if (Time.time - lastHeal > helaingCoolDown)
+        if (Time.time - lastHeal > helaingCoolDown) // if the player is detected  it will run down the cooldown timer and heal the player by a set amount and reset the timer 
         {
             lastHeal = Time.time;
             GameManager.instance.player.Heal(healingAmount);
