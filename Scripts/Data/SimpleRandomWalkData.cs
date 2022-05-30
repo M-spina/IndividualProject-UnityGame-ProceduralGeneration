aef634dd0a06e40595ea3ac305902bd0b82bedcb
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 18:19:45 +0100

    Update SimpleRandomWalkData.cs

--- Scripts/Data/SimpleRandomWalkData.cs
+++ Scripts/Data/SimpleRandomWalkData.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 [CreateAssetMenu(fileName = "SimpleRandomWalkParameters_",menuName = "PCG/SimpleRandomWalkData")]
+// this class hold a pointer reference to parameter that we would be using in our pgc alorithm so that when we create muiltiple agents we can just point to the clas that hold the peramters
 
 public class SimpleRandomWalkData : ScriptableObject
 {
