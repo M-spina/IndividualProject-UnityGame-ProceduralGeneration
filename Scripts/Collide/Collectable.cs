c7716e8b86fefbcc16b4f8b1377483912acdf624
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 18:08:04 +0100

    Update Collectable.cs

--- Scripts/Collide/Collectable.cs
+++ Scripts/Collide/Collectable.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// this class inherits form Collidable that handles collison for the object 
 public class Collectable : Collidable
 {
     protected bool collected; // private to all but the child classes 
 
-    protected override void OnCollide(Collider2D coll)
-    {
+    protected override void OnCollide(Collider2D coll) // has a overriden method that can be called by child class  
+    { //this  method test to see if an object in the collision array has the name of a player and if it is then it calls the Oncollect method
         if (coll.name == "Player")
         {
             OnCollect();
@@ -15,7 +15,7 @@ public class Collectable : Collidable
         }
     }
 
-    protected virtual void OnCollect()
+    protected virtual void OnCollect() // this method just chnage the bool to true telling a child class that the object can be collected.
     {
         collected = true;
     }
