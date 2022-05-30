f9056c1a43e5a4ebd73d86f535f4140ae59a4a39
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 19:55:06 +0100

    Update Fighter.cs

--- Scripts/Fighter/Fighter.cs
+++ Scripts/Fighter/Fighter.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// this class is used to ceate metods that could damage and destroy an object to fighting object such as player  enemis and destroyable objects
 
 public class Fighter : MonoBehaviour
 {
@@ -16,18 +17,18 @@ public class Fighter : MonoBehaviour
     //Push 
     protected Vector3 pushDirection;
     
-    // fighters can receive damage / die
+    // fighters can receive damage by checking if their invinciblity timer is up this is ot prevent damage form contantly beeing recived 
     protected virtual void ReceiveDamage(Damage dmg)
     {
         if (Time.time - LastInvincable > InvinciblityTime)
         {
-            LastInvincable = Time.time;
+            LastInvincable = Time.time; // reset the invinciblity timer and reduce the object hitpoints 
             hitpoint -= dmg.damageAmount;
             pushDirection = (transform.position - dmg.origin).normalized * dmg.pushForce; // the vector between the receiver and the sender
-            // multiplied by the push force 
-            
+            // multiplied by the push force  to push the object that recived damage away
+            // display text that the object has been damaged 
             GameManager.instance.ShowText(dmg.damageAmount.ToString(),25,Color.red, transform.position,Vector3.up*20,0.5f);
-
+            // if hit poiyns hit zero the class calls the Death method
             if (hitpoint <= 0)
             {
                 hitpoint = 0;
@@ -35,7 +36,7 @@ public class Fighter : MonoBehaviour
             }
         }
     }
-
+   // inherited class can override this method do somthing different 
     protected virtual void Death()
     {
         
