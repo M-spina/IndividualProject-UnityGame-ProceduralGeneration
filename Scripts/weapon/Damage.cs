463a52bb252db3afa37f3e2d5917ab1c4b558abd
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 22:31:22 +0100

    Update Damage.cs

--- Scripts/weapon/Damage.cs
+++ Scripts/weapon/Damage.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-// this class is used to store variables to be used in the weapon class 
+// this class is used to store variables  for the damage object that is used to damage game objects and push them around.
 public class Damage : MonoBehaviour
 {
     public Vector3 origin; // position of the receiver
