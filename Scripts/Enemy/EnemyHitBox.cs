e518dac034c98e7ca213674537fdd9e242784b2a
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 18:46:38 +0100

    Update EnemyHitBox.cs

--- Scripts/Enemy/EnemyHitBox.cs
+++ Scripts/Enemy/EnemyHitBox.cs
@@ -1,16 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+// this class inherits form Collidable so that when it collides with an object it will report it 
 public class EnemyHitBox : Collidable
 {
    //Damage
    public int damage;
    public float pushForce;
 
-   protected override void OnCollide(Collider2D coll)
-   {
-       // if goes bad this prob problem
+   protected override void OnCollide(Collider2D coll) // we override the base method and instead of reporting it will run a custom method thet would create a damage object  
+   { // this would be taken in by the player and the dmg report is detroyed 
+       
       if (coll.CompareTag("Player") && coll.name == "Player")
       {
        // Create a Damage object , before sending it to the player
