ae6b3b6f5a76d8b58668f5a92fa5bdf75f5f6d12
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 17:57:13 +0100

    Update Camera_Movement.cs

--- Scripts/Camera/Camera_Movement.cs
+++ Scripts/Camera/Camera_Movement.cs
@@ -10,14 +10,13 @@ public class Camera_Movement : MonoBehaviour
     {
         target = GameObject.Find("Player").transform;
 
-        //target = GameObject.FindGameObjectWithTag("Fighter").GetComponent<Transform>();
+ 
         // are target variable would get the game-object with the tag transform coordinates
     }
 
     private void LateUpdate() // late update is called after all update funtions are called used to order script executions 
     {
-        //MARKER method 1 traditional method to move the camera
-        //transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+        
         // updates the cameras position with the targets x,y coordinates. We cant use the target's z as i wouldn't be able to see the player  
         
         
