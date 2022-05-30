07a5f9c2eb6f5933b748fc06a8fd5b024ffaf7a6
Malcolm Spina <61895188+M-spina@users.noreply.github.com>
2022-05-30 18:21:41 +0100

    Update DontDestroyOnLoad.cs

--- Scripts/DontDestroyOnLoad/DontDestroyOnLoad.cs
+++ Scripts/DontDestroyOnLoad/DontDestroyOnLoad.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+// this class is used to pervent object form being destroyed when trasitioning to a different scene in the game
 
 public class DontDestroyOnLoad : MonoBehaviour
 {
